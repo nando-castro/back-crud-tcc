614b62c3594549c5aee5ed38568fe15d0f362ea5
fernando <sousa.fernandocastro@gmail.com>
2023-10-03 09:58:38 -0300

    :sparkles: feat: adicionado cors

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
