51d1d406f207c59e904a7f7715a12726fa9b8cd7
nando-castro <fernando.castro10.master@gmail.com>
2023-12-02 23:13:49 -0300

    :sparkles: feat: retorno de erro 404

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   if (users.length > 0) return res.status(200).send(users);
-  return res.send("No users found");
+  return res.status(404).send("No user found");
 });
 
 app.post("/user", async (req, res) => {
@@ -60,7 +60,7 @@ app.get("/users/:name", async (req, res) => {
     },
   });
   if (user.length > 0) return res.status(200).send(user);
-  return res.send("No users found");
+  return res.status(404).send("No user found");
 });
 
 app.get("/user/:id", async (req, res) => {
